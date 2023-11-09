d54ae11e88156bcff8e3869392d4ae0372b29546
ramonnery <ramonnery2020@gmail.com>
2023-11-09 19:31:51 -0300

    iniciando o algoritimo da opcao1

--- financas_bot.py
+++ financas_bot.py
@@ -3,14 +3,32 @@ import telebot
 CHAVE_API = '6493137670:AAHBijEnyT4QKfiyzEDb5q-X-M_jlraWSao'
 bot = telebot.TeleBot(CHAVE_API)
 
-@bot.message_handler(func= lambda msg: True)
+orcamento_semanal = 0.0
+
+@bot.message_handler(commands=['opcao1'])
+def opcao1(mensagem):
+    
+    texto = """
+Ok! Para qual valor você deseja atualizar? Use "." em vez de "," caso seja um valor quebrado. (exemplo: 142.5)
+"""
+    bot.send_message(mensagem.chat.id, texto)
+
+@bot.message_handler(commands=['opcao2'])
+def opcao2(mensagem):
+    pass
+
+@bot.message_handler(func= lambda _: True)
 def responder(mensagem):
 
-    texto_inicial =  '''
-Olá, aqui é o Bot de Finanças criado por Ramon Nery. Vou te ajudar a controlar melhor seu dinheiro no decorrer da semana.
+    print(mensagem)
+
+    user_name = mensagem.from_user.first_name
+
+    texto_inicial =  f'''
+Olá, {user_name}! Meu nome é Finanças. Vou te ajudar a controlar melhor seu dinheiro no decorrer da semana. Segundo meu banco de dados, seu orçamento semanal atual está no valor de R$ {orcamento_semanal}
     
     Para iniciarmos, clique em um dos itens abaixo:
-    /opcao1 Atualizar o valor de seu orçamento semanal
+    /opcao1 Atualizar o valor do seu orçamento semanal
     /opcao2 Adicionar algum gasto
 
 ⚠ Responder qualquer outra coisa não vai funcionar.
